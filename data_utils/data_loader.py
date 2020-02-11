c9d2491b0ffd2a80c0ca13a3f2633697b287b0c7
萩原辰哉 <hagiharatatsuya@hagiharatatsuyanoMacBook-Pro.local>
2020-02-11 19:23:37 +0900

    update

---
+++ data_utils/data_loader.py
@@ -0,0 +1,83 @@
+import os
+import sys
+import tarfile
+import collections
+import torch.utils.data as data
+import shutil
+import numpy as np
+from natsort import natsorted
+from PIL import Image
+
+
+def voc_cmap(N=256, normalized=False):
+    def bitget(byteval, idx):
+        return ((byteval & (1 << idx)) != 0)
+
+    dtype = 'float32' if normalized else 'uint8'
+    cmap = np.zeros((N, 3), dtype=dtype)
+    for i in range(N):
+        r = g = b = 0
+        c = i
+        for j in range(8):
+            r = r | (bitget(c, 0) << 7-j)
+            g = g | (bitget(c, 1) << 7-j)
+            b = b | (bitget(c, 2) << 7-j)
+            c = c >> 3
+
+        cmap[i] = np.array([r, g, b])
+
+    cmap = cmap/255 if normalized else cmap
+    return cmap
+
+class VOCSegmentation(data.Dataset):
+    """`Pascal VOC <http://host.robots.ox.ac.uk/pascal/VOC/>`_ Segmentation Dataset.
+    Args:
+        root (string): Root directory of the VOC Dataset.
+        year (string, optional): The dataset year, supports years 2007 to 2012.
+        image_set (string, optional): Select the image_set to use, ``train``, ``trainval`` or ``val``
+        download (bool, optional): If true, downloads the dataset from the internet and
+            puts it in root directory. If dataset is already downloaded, it is not
+            downloaded again.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.RandomCrop``
+    """
+    cmap = voc_cmap()
+    def __init__(self, 
+                 image_dir, 
+                 mask_dir, 
+                 image_set='train',
+                 transform=None):
+        
+        self.transform = transform
+        self.image_set = image_set
+        # jpg image
+        load_img_path = [os.path.join(image_dir, path) for path in os.listdir(image_dir)]
+        self.images =[path for path in natsorted(load_img_path)]
+        # png mask
+        load_mask_path = [os.path.join(mask_dir, path) for path in os.listdir(mask_dir)]
+        self.masks =[path for path in natsorted(load_mask_path)]
+        assert (len(self.images) == len(self.masks))
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is the image segmentation.
+        """
+        img = Image.open(self.images[index]).convert('RGB')
+        target = Image.open(self.masks[index])
+        target = target.convert('L')
+        if self.transform is not None:
+            img, target = self.transform(img, target)
+
+        return img, target
+
+
+    def __len__(self):
+        return len(self.images)
+
+    @classmethod
+    def decode_target(cls, mask):
+        """decode semantic mask to RGB image"""
+        return cls.cmap[mask]
